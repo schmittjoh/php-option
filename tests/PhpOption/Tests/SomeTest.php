de52429d38c2687d0456fc9963275e585913bb46
Lars Strojny <lars@strojny.net>
2012-11-11 20:08:01 +0100

    Adding Some::create() method to keep API consistent with None()

--- tests/PhpOption/Tests/SomeTest.php
+++ tests/PhpOption/Tests/SomeTest.php
@@ -12,4 +12,13 @@ class SomeTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('foo', $some->getOrCall('does_not_exist'));
         $this->assertFalse($some->isEmpty());
     }
+
+    public function testCreate()
+    {
+        $some = \PhpOption\Some::create('foo');
+        $this->assertEquals('foo', $some->get());
+        $this->assertEquals('foo', $some->getOrElse(null));
+        $this->assertEquals('foo', $some->getOrCall('does_not_exist'));
+        $this->assertFalse($some->isEmpty());
+    }
 }
