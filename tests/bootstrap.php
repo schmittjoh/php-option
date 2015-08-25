598199307b4d752951f1954e6240a3acc2504782
Ilya Bakhlin <ilya@bakhlin.com>
2015-08-25 21:02:44 +0200

    Improving the if control statement.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,6 @@
 if ( ! is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
     echo 'Could not find "vendor/autoload.php". Did you forget to run "composer install --dev"?'.PHP_EOL;
     exit(1);
+} else {
+    require_once $autoloadFile;
 }
-
-require_once $autoloadFile;
