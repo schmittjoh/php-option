3297bbbcb8f464de98ff73088fb4079ca97bd193
Ilya Bakhlin <ilya@bakhlin.com>
2015-08-25 21:28:27 +0200

    Improving the OptionTest.php file's code.

--- tests/PhpOption/Tests/OptionTest.php
+++ tests/PhpOption/Tests/OptionTest.php
@@ -2,23 +2,24 @@
 
 namespace PhpOption\Tests;
 
+use PhpOption\LazyOption;
 use PhpOption\None;
 use PhpOption\Option;
 use PhpOption\Some;
 
 class OptionTest extends \PHPUnit_Framework_TestCase
 {
-    public function testfromValueWithDefaultNoneValue()
+    public function testFromValueWithDefaultNoneValue()
     {
-        $this->assertInstanceOf('PhpOption\None', \PhpOption\Option::fromValue(null));
-        $this->assertInstanceOf('PhpOption\Some', \PhpOption\Option::fromValue('value'));
+        $this->assertInstanceOf('PhpOption\None', Option::fromValue(null));
+        $this->assertInstanceOf('PhpOption\Some', Option::fromValue('value'));
     }
 
     public function testFromValueWithFalseNoneValue()
     {
-        $this->assertInstanceOf('PhpOption\None', \PhpOption\Option::fromValue(false, false));
-        $this->assertInstanceOf('PhpOption\Some', \PhpOption\Option::fromValue('value', false));
-        $this->assertInstanceOf('PhpOption\Some', \PhpOption\Option::fromValue(null, false));
+        $this->assertInstanceOf('PhpOption\None', Option::fromValue(false, false));
+        $this->assertInstanceOf('PhpOption\Some', Option::fromValue('value', false));
+        $this->assertInstanceOf('PhpOption\Some', Option::fromValue(null, false));
     }
 
     public function testFromArraysValue()
@@ -36,25 +37,25 @@ class OptionTest extends \PHPUnit_Framework_TestCase
         $false = function() { return false; };
         $some = function() { return 'foo'; };
 
-        $this->assertTrue(\PhpOption\Option::fromReturn($null)->isEmpty());
-        $this->assertFalse(\PhpOption\Option::fromReturn($false)->isEmpty());
-        $this->assertTrue(\PhpOption\Option::fromReturn($false, array(), false)->isEmpty());
-        $this->assertTrue(\PhpOption\Option::fromReturn($some)->isDefined());
-        $this->assertFalse(\PhpOption\Option::fromReturn($some, array(), 'foo')->isDefined());
+        $this->assertTrue(Option::fromReturn($null)->isEmpty());
+        $this->assertFalse(Option::fromReturn($false)->isEmpty());
+        $this->assertTrue(Option::fromReturn($false, array(), false)->isEmpty());
+        $this->assertTrue(Option::fromReturn($some)->isDefined());
+        $this->assertFalse(Option::fromReturn($some, array(), 'foo')->isDefined());
     }
 
     public function testOrElse()
     {
-        $a = new \PhpOption\Some('a');
-        $b = new \PhpOption\Some('b');
+        $a = new Some ('a');
+        $b = new Some ('b');
 
         $this->assertEquals('a', $a->orElse($b)->get());
     }
 
     public function testOrElseWithNoneAsFirst()
     {
-        $a = \PhpOption\None::create();
-        $b = new \PhpOption\Some('b');
+        $a = None::create();
+        $b = new Some ('b');
 
         $this->assertEquals('b', $a->orElse($b)->get());
     }
@@ -63,18 +64,18 @@ class OptionTest extends \PHPUnit_Framework_TestCase
     {
         $throws = function() { throw new \LogicException('Should never be called.'); };
 
-        $a = new \PhpOption\Some('a');
-        $b = new \PhpOption\LazyOption($throws);
+        $a = new Some ('a');
+        $b = new LazyOption($throws);
 
         $this->assertEquals('a', $a->orElse($b)->get());
     }
 
     public function testOrElseWithMultipleAlternatives()
     {
-        $throws = new \PhpOption\LazyOption(function() { throw new \LogicException('Should never be called.'); });
-        $returns = new \PhpOption\LazyOption(function() { return new \PhpOption\Some('foo'); });
+        $throws = new LazyOption(function() { throw new \LogicException('Should never be called.'); });
+        $returns = new LazyOption(function() { return new Some ('foo'); });
 
-        $a = \PhpOption\None::create();
+        $a = None::create();
 
         $this->assertEquals('foo', $a->orElse($returns)->orElse($throws)->get());
     }
