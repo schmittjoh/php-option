56030e5c56c0feafb7f75a6b0febd7e59aadf506
Matthew Brown <github@muglug.com>
2019-12-14 18:18:04 -0500

    Fix name

--- src/PhpOption/LazyOption.php
+++ src/PhpOption/LazyOption.php
@@ -97,7 +97,7 @@ final class LazyOption extends Option
 
     public function ifDefined($callable)
     {
-        $this->option()->isDefined($callable);
+        $this->option()->ifDefined($callable);
     }
 
     public function forAll($callable)
